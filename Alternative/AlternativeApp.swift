93be051ecdadcbe64fbadaf95ba81bce959b69b9
EugeneMazurok <audiofaun896373@gmail.com>
2024-03-13 00:00:26 +0300

    Initial Commit

---
+++ Alternative/AlternativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlternativeApp.swift
+//  Alternative
+//
+//  Created by Евгений Мазурок on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AlternativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
